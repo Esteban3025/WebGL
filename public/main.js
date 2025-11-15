6b2cf74b556ca077d8703295622532bf19927201
nosekp <glo3025pando@gmail.com>
2025-11-15 10:14:07 -0300

    angle change

--- public/main.js
+++ public/main.js
@@ -8,8 +8,9 @@ function main () {
     throw new Error('No se pudo iniciar webgl');
   }
 
-  const translation = [0, 0];
+  const translation = [100, 150];
   const rotation = [0, 1];
+  let angleInRadians = 0;
   const scale = [1, 1];
   const width = 100;
   const height = 30;
@@ -24,20 +25,19 @@ function main () {
   const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
   const resoluctionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
   const colorUniformLocation = gl.getUniformLocation(program, 'u_color'); // HACER RELACION CON EL FRAGMENT SHADER
-  const translationLocation = gl.getUniformLocation(program, 'u_translation');
-  const rotationLocation = gl.getUniformLocation(program, 'u_rotation');
-  const scaleLocation = gl.getUniformLocation(program, 'u_scale');
+  const matrixLocation = gl.getUniformLocation(program, "u_matrix");
+  
   
   const positionBuffer = gl.createBuffer(); // UN ESPACIO EN MEMORIA
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); // BINDING EL ESPACIO DE MEMORIA A UN BUFFER DE LA GPU
-  setGeometry(gl);
+  setGeometry(gl); // BUffer data - datos a enviar al buffer.
 
   drawScene();
 
   webglLessonsUI.setupSlider("#x", {slide: updatePosition(0), max: gl.canvas.width });
   webglLessonsUI.setupSlider("#y", {slide: updatePosition(1), max: gl.canvas.height});
   webglLessonsUI.setupSlider("#angle", {slide: updateAngle, max: 360});
-   webglLessonsUI.setupSlider("#scaleX", {value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2});
+  webglLessonsUI.setupSlider("#scaleX", {value: scale[0], slide: updateScale(0), min: -5, max: 5, step: 0.01, precision: 2});
   webglLessonsUI.setupSlider("#scaleY", {value: scale[1], slide: updateScale(1), min: -5, max: 5, step: 0.01, precision: 2});
 
   function updatePosition(index) {
@@ -56,18 +56,18 @@ function main () {
 
   function updateAngle(event, ui) {
     const angleInDegrees = 360 - ui.value;
-    const angleInRadians = angleInDegrees * Math.PI / 180;
-    rotation[0] = Math.sin(angleInRadians);
-    rotation[1] = Math.cos(angleInRadians);
+    angleInRadians = angleInDegrees * Math.PI / 180;
     drawScene();
   }
-
-  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW); // PASANDO DATOS AL BUFFER
 
   function drawScene() {
     webglUtils.resizeCanvasToDisplaySize(gl.canvas); 
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    const translationMatrix = m3.translation(translation[0], translation[1]);
+    const rotationMatrix = m3.rotation(angleInRadians);
+    const scaleMatrix = m3.scaling(scale[0], scale[1]);
 
     gl.clearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a); // COLOR DEL FONDO
     gl.clear(gl.COLOR_BUFFER_BIT); // LIMPIAR EL COLOR DEL FONDO
@@ -75,19 +75,93 @@ function main () {
     gl.useProgram(program);
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.uniform2f(resoluctionUniformLocation, gl.canvas.width, gl.canvas.height);
-  
+
     gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
     gl.uniform2f(resoluctionUniformLocation, gl.canvas.width, gl.canvas.height);
     gl.uniform4fv(colorUniformLocation, color);
-    gl.uniform2fv(translationLocation, translation);
-    gl.uniform2fv(rotationLocation, rotation);
-    gl.uniform2fv(scaleLocation, scale);
+
+    const moveOriginMatrix = m3.translation(-50, -75);
+
+    let matrix = m3.identity();
+
+    matrix = m3.multiply(matrix, translationMatrix);
+    matrix = m3.multiply(matrix, scaleMatrix);
+    matrix = m3.multiply(matrix, rotationMatrix);
+
+    gl.uniformMatrix3fv(matrixLocation, false, matrix); // set the matrix
+
     gl.drawArrays(gl.TRIANGLES, 0, 18);  
   }
-}
+};
 
-main();
+const m3 = {
+  translation: function(tx, ty) {
+    return [
+      1, 0, 0,
+      0, 1, 0,
+      tx, ty, 1
+    ];
+  },
+
+  rotation: function(angleInRadians) {
+    const c = Math.cos(angleInRadians);
+    const s = Math.sin(angleInRadians);
+    return [
+      c, -s, 0,
+      s, c, 0,
+      0, 0, 1
+    ];
+  },
+
+  scaling: function(sx, sy) {
+    return [
+      sx, 0, 0,
+      0, sy, 0,
+      0, 0, 1
+    ]
+  },
+
+  identity: function() {
+    return [
+      1, 0, 0,
+      0, 1, 0,
+      0, 0, 1,
+    ];
+  },
+
+  multiply: function(a, b) {
+    var a00 = a[0 * 3 + 0];
+    var a01 = a[0 * 3 + 1];
+    var a02 = a[0 * 3 + 2];
+    var a10 = a[1 * 3 + 0];
+    var a11 = a[1 * 3 + 1];
+    var a12 = a[1 * 3 + 2];
+    var a20 = a[2 * 3 + 0];
+    var a21 = a[2 * 3 + 1];
+    var a22 = a[2 * 3 + 2];
+    var b00 = b[0 * 3 + 0];
+    var b01 = b[0 * 3 + 1];
+    var b02 = b[0 * 3 + 2];
+    var b10 = b[1 * 3 + 0];
+    var b11 = b[1 * 3 + 1];
+    var b12 = b[1 * 3 + 2];
+    var b20 = b[2 * 3 + 0];
+    var b21 = b[2 * 3 + 1];
+    var b22 = b[2 * 3 + 2];
+    return [
+      b00 * a00 + b01 * a10 + b02 * a20,
+      b00 * a01 + b01 * a11 + b02 * a21,
+      b00 * a02 + b01 * a12 + b02 * a22,
+      b10 * a00 + b11 * a10 + b12 * a20,
+      b10 * a01 + b11 * a11 + b12 * a21,
+      b10 * a02 + b11 * a12 + b12 * a22,
+      b20 * a00 + b21 * a10 + b22 * a20,
+      b20 * a01 + b21 * a11 + b22 * a21,
+      b20 * a02 + b21 * a12 + b22 * a22,
+    ];
+  },
+}
 
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
@@ -120,6 +194,8 @@ function randomInt(range) {
   return Math.floor(Math.random() * range);
 }
 
+
+
 function setGeometry(gl) {
   gl.bufferData(
     gl.ARRAY_BUFFER,
@@ -147,3 +223,5 @@ function setGeometry(gl) {
           67, 90,
   ]), gl.STATIC_DRAW);
 }
+
+main();

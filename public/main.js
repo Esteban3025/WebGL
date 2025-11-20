1403878d1aff40aec824cc59ff57a616f162a313
nosekp <glo3025pando@gmail.com>
2025-11-20 13:57:17 -0300

    restart everything xdd

--- public/main.js
+++ public/main.js
@@ -1,142 +1,74 @@
-import { Shapes } from "./tools/Shapes.js";
+function main() {
+  const canvas = document.querySelector('#canvas');
+  const gl = canvas.getContext('webgl');
+  if (!gl) {
+    return;
+  }
 
-function main () {
-  const shape = new Shapes();
-  shape.draw({
-    positions: [
-      // left column
-            0,   0,  0,
-           30,   0,  0,
-            0, 150,  0,
-            0, 150,  0,
-           30,   0,  0,
-           30, 150,  0,
- 
-          // top rung
-           30,   0,  0,
-          100,   0,  0,
-           30,  30,  0,
-           30,  30,  0,
-          100,   0,  0,
-          100,  30,  0,
- 
-          // middle rung
-           30,  60,  0,
-           67,  60,  0,
-           30,  90,  0,
-           30,  90,  0,
-           67,  60,  0,
-           67,  90,  0,
-    ],
-    color: [5, 0, 0, 1],
-  });
+  const vertexShaderSource = document.querySelector("#vertex-shader").textContent;
+  const fragmentShaderSource = document.querySelector("#fragment-shader").textContent;
 
-  shape.drawScene();
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-  // function drawScene(now) {
-  //   now *= 0.001;
-  //   let deltaTime = now - then;
+  const program = createProgram(gl, vertexShader, fragmentShader);
 
-  //   webglUtils.resizeCanvasToDisplaySize(gl.canvas); 
+  const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+  const positionBuffer = gl.createBuffer();
 
-  //   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  //   gl.clearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a); // COLOR DEL FONDO
-  //   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // LIMPIAR EL COLOR DEL FONDO
+  const positions = [
+    0, 0,
+    0, 0.5,
+    0.7, 0,
+  ];
 
-  //   modelYRotationRadians += -0.7 * deltaTime;
-  //   modelXRotationRadians += -0.4 * deltaTime;
-    
-  //   gl.useProgram(program);
-  //   gl.enableVertexAttribArray(positionAttributeLocation);
-  //   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  //   gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-  //   gl.enableVertexAttribArray(texcoordLocation);
-  //   gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-  //   gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0);
-    
-  //   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-  //   const projectionMatrix =
-  //       m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
-  //   const cameraPosition = [0, 0, 2];
-  //   const up = [0, 1, 0];
-  //   const target = [0, 0, 0];
+  gl.useProgram(program);
 
-  //   // Compute the camera's matrix using look at.
-  //   const cameraMatrix = m4.lookAt(cameraPosition, target, up);
+  gl.enableVertexAttribArray(positionAttributeLocation);
 
-  //   // Make a view matrix from the camera matrix.
-  //   const viewMatrix = m4.inverse(cameraMatrix);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  //   const viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-  //   let matrix = m4.xRotate(viewProjectionMatrix, modelXRotationRadians);
-  //   matrix = m4.yRotate(matrix, modelYRotationRadians);
-
-    
-  //   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
-  //   gl.uniform1i(textureLocation, 0);
-  //   gl.uniformMatrix4fv(matrixLocation, false, matrix); // set the matrix
-  //   gl.drawArrays(gl.TRIANGLES, 0, 16 * 6); 
-  //   then = now;
-  //   requestAnimationFrame(drawScene);  
-  // }
-};
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
 
 
+function createShader(gl, type, source) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
 
-function setTexcoords(gl) {
-  gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array([
-        // select the top left image
-    0   , 0  ,
-    0   , 0.5,
-    0.25, 0  ,
-    0   , 0.5,
-    0.25, 0.5,
-    0.25, 0  ,
-    // select the top middle image
-    0.25, 0  ,
-    0.5 , 0  ,
-    0.25, 0.5,
-    0.25, 0.5,
-    0.5 , 0  ,
-    0.5 , 0.5,
-    // select to top right image
-    0.5 , 0  ,
-    0.5 , 0.5,
-    0.75, 0  ,
-    0.5 , 0.5,
-    0.75, 0.5,
-    0.75, 0  ,
-    // select the bottom left image
-    0   , 0.5,
-    0.25, 0.5,
-    0   , 1  ,
-    0   , 1  ,
-    0.25, 0.5,
-    0.25, 1  ,
-    // select the bottom middle image
-    0.25, 0.5,
-    0.25, 1  ,
-    0.5 , 0.5,
-    0.25, 1  ,
-    0.5 , 1  ,
-    0.5 , 0.5,
-    // select the bottom right image
-    0.5 , 0.5,
-    0.75, 0.5,
-    0.5 , 1  ,
-    0.5 , 1  ,
-    0.75, 0.5,
-    0.75, 1,
-      ]),
-      gl.STATIC_DRAW);
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
 }
 
 main();
